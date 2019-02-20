d9ae3b7ba053751840646873c27e0ba9ebeba94e
Ho Yin SHIH <hyshih@connect.ust.hk>
2019-02-21 03:24:22 +0800

    Processing...

---
+++ Ruby v1.0/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Ruby
+{
+    class Loader
+    {
+        public static void Init()
+        {
+            new Main.Load();
+        }
+
+        private static GameObject Load;
+
+    }
+}
---
+++ Ruby v1.0/Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFT;
+using UnityEngine;
+
+namespace Ruby
+{
+    public class Main : MonoBehaviour
+    {
+        private GameObject hGameObj;
+        private Player[] playerList;
+
+        private float nextUpdateTime;
+        private float updateInterval = 15f;
+
+        private bool bESP = false;
+
+        private float playerUpdateInterval = 500f;
+        private float viewDistance = 1200f;
+
+        private Player localPlayer;
+        private Vector3 camPos;
+
+        private void Awake()
+        {
+
+        }
+
+        private void Start()
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            playerList = null;
+            localPlayer = null;
+            nextUpdateTime = 0f;
+        }
+
+        private void OnEnable()
+        {
+            Clear();
+        }
+
+        private void OnDisable()
+        {
+            Clear();
+        }
+
+        private void OnDestroy()
+        {
+            Clear();
+        }
+
+        public void Load()
+        {
+            hGameObj = new GameObject();
+            hGameObj.AddComponent<Main>();
+            DontDestroyOnLoad(hGameObj);
+        }
+
+        public void Unload()
+        {
+            try
+            {
+                Clear();
+                Destroy(hGameObj);
+                Destroy(hGameObj.GetComponent<Main>());
+                Destroy(this);
+                DestroyObject(this);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.End))
+            {
+                Unload();
+            }
+
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                bESP = !bESP;
+            }
+        }
+
+        private void UpdateLocalPlayer()
+        {
+            playerList = FindObjectsOfType<Player>();
+        }
+
+
+    }
+}
